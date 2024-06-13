b02619637d1fc2ab9631b53f7b88ed2ebe3eb5f1
chenmch <chenmch@diit.cn>
2024-06-13 15:56:07 +0800

    plugins: remove codeium add fittencode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -161,13 +161,20 @@ return {
     'kevinhwang91/rnvimr',
   },
   {
-    'Exafunction/codeium.vim',
+    'luozhiya/fittencode.nvim',
+    lazy = false,
     config = function()
-      -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set('i', '<M-l>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-      vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-      vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-      vim.keymap.set('i', '<M-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-    end
+      require('fittencode').setup({
+        keymaps = {
+          inline = {
+            ['<TAB>'] = 'accept_all_suggestions',
+            ['<A-l>'] = 'accept_line',
+            ['<C-UP>'] = 'revoke_line',
+            ['<C-Left>'] = 'revoke_word',
+            ['<A-\\>'] = 'triggering_completion',
+          },
+        }
+      })
+    end,
   },
 }

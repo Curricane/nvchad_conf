85527f99a615e0f5646c18c0dbaa45a18dcca155
curricane <curricanechen@yunify.com>
2024-06-15 11:58:01 +0800

    keymap: remap some key similar to lunarvim

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,20 +1,40 @@
 require "nvchad.mappings"
 
--- delete key map
+---- [delete key map]
 local nomap = vim.keymap.del
 nomap("n", "<A-i>")
+-- global lsp mappings
+nomap("n", "<leader>ds")
+-- tabufline new buf
+nomap("n", "<leader>b")
+-- nvimtree NvimTreeToggle
+nomap("n", "<C-n>")
 -- nomap("t", "<A-i")
 
--- add yours here
+-- telescope
+nomap("n", "<leader>fw")
+nomap("n", "<leader>fb")
+nomap("n", "<leader>fh")
+nomap("n", "<leader>ma")
+nomap("n", "<leader>fo")
+nomap("n", "<leader>fz")
+nomap("n", "<leader>cm")
+nomap("n", "<leader>gt")
+nomap("n", "<leader>pt")
+nomap("n", "<leader>th")
+nomap("n", "<leader>ff")
+nomap("n", "<leader>fa")
+nomap("n", "<leader>fm")
 
+-------------------- add yours here
 local map = vim.keymap.set
 
---- float Terminal
+-- float Terminal
 map({ "n", "t" }, "<A-3>", function()
   require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
 end, { desc = "Terminal Toggle Floating term" })
 
---- move
+-- move
 map("n", "<A-k>", ":m .-2<CR>==", {desc = "Move current line / block with Alt-j/k ala vscode"})
 map("n", "<A-j>", ":m .+1<CR>==", {desc = "Move current line / block with Alt-j/k ala vscode"})
 
@@ -22,6 +42,7 @@ map("n", "<A-j>", ":m .+1<CR>==", {desc = "Move current line / block with Alt-j/
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", {desc = "Explorer"})
 
 
+-- goto-preview
 map('n', 'gpd', "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { desc = "preview definition" })
 map('n', 'gpt', "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", { desc = "preview type" })
 map('n', 'gpi', "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>",{ desc = "previm impl" })
@@ -80,3 +101,21 @@ map('n', '<leader>l', '', { desc = 'lsp'})
 map('n', '<leader>lS', '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>', { desc = 'Workspace Symbols'})
 
 -- hop keymap see init.lua
+
+
+-- buffer
+map("n", "<leader>b", "", { desc = "Buffers" })
+map("n", "<leader>bf", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+
+-- search
+map("n", "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
+map('n', "<leader>s", "", {desc = "Search"})
+map("n", "<leader>st", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
+map(
+  "n",
+  "<leader>sf",
+  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+  { desc = "telescope find all files" }
+)
+-- marks
+map("n", "<leader>sm", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })

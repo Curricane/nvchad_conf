bc5219fb0a316be3c80ffe2d2de4389826dceef2
chenmc <chenmch@diit.cn>
2024-08-01 13:48:18 +0800

    update nvchad

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,8 +2,13 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
     config = function()
-      require "configs.conform"
+      require "configs.lspconfig"
     end,
   },
 

755a9f74a1555980df004ec0d78f3d3b0ddeed24
chenmc <chenmch@diit.cn>
2024-09-02 15:28:18 +0800

    remove incorrect config on lsp to fix goto-preview display dulicate window

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = {  "rust_analyzer", "gopls", "golangci_lint_ls", "lua_ls" }
+local servers = { "lua_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
